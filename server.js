a0485731eb658690c12f5326fd3a7e2e0948b37d
Marquell Thompson <77359365+mqt2999@users.noreply.github.com>
2021-07-28 18:38:47 -0500

    adding more

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname, '../client/build', 'index.html'));
+  app.use(express.static(__dirname, 'client/build'));
 }
 // Add routes, both API and view
 app.use(routes);
