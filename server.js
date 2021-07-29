db92edd41778e0af9270c91a92da0bd053dc4b30
Marquell Thompson <77359365+mqt2999@users.noreply.github.com>
2021-07-28 19:16:00 -0500

    I think this fixed it

--- server.js
+++ server.js
@@ -10,13 +10,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname, 'client/build'));
+  app.use(express.static(__dirname, '../client/build'));
 }
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks")
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
 
 // Start the API server
 app.listen(PORT, function() {
